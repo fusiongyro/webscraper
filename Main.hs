f9519d8d0cc7fa5578ca1d82c020da06b1660c42
Daniel Lyons <fusion@storytotell.org>
2012-03-21 22:14:03 -0600

    parallel fetching operational

--- Main.hs
+++ Main.hs
@@ -1,25 +1,36 @@
 module Main where
 
+import Control.Applicative ((<$>))
 import Data.Maybe
 import System.IO
 import System.Environment
 
+import Control.Concurrent.ParallelIO
 import Network.URI
 
 import Parse
 import Fetch
 
+-- | Download all the images linked to in the document at this URI.
 crawl :: URI -> IO ()
 crawl rootURL = do
+  -- fetch the page
+  putStrLn $ "processing " ++ show rootURL
   pageContent <- fetchUrl rootURL
+  
+  -- get all the image links as URIs
   let images = mapMaybe (`relativeTo` rootURL) $ imageLinks pageContent
-  mapM_ fetchImage images
+  
+  -- fetch each image
+  parallel_ $ map fetchImage images
     where
       fetchImage image = do
-        putStrLn $ "fetching " ++ (show image)
+        putStrLn $ "  fetching " ++ show image
         downloadUrl image
 
+-- | Treat each command line argument as a top-level URI and download the 
+-- images therefrom.
 main = do
-  args <- getArgs
-  let validURIs = mapMaybe parseURI args
+  validURIs <- mapMaybe parseURI <$> getArgs
   mapM_ crawl validURIs
+  stopGlobalPool
--- Parse.hs
+++ Parse.hs
@@ -6,22 +6,26 @@ import Data.Maybe (mapMaybe)
 import Network.URI
 import Text.HTML.TagSoup
 
+-- | If the tag has this name, Just this tag, else Nothing.
 tagNamed :: String -> Tag String -> Maybe (Tag String)
 tagNamed name t@(TagOpen tagName _) = if name == tagName 
     then Just t 
     else Nothing
 tagNamed name _ = Nothing
 
+-- | If the tag has attributes, Just the attributes, otherwise Nothing.
 tagAttributes :: Tag String -> Maybe [Attribute String]
 tagAttributes (TagOpen _ attributes) = Just attributes
 tagAttributes _                      = Nothing
 
+-- | Just the 'src' attribute of an <img> tag, or Nothing
 imageSource :: Tag String -> Maybe String
 imageSource tag = do
   imageTag <- tagNamed "img" tag
   attrs <- tagAttributes imageTag
   lookup "src" attrs
 
+-- | Ferret out the URIs in all the image tags in the supplied document.
 imageLinks :: String -> [URI]
 imageLinks = mapMaybe parseURIReference . process . parseTags
   where
