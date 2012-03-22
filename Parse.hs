29d60b5b65818908fdeb77104dc0fe475bfbbc8b
Daniel Lyons <fusion@storytotell.org>
2012-03-21 19:53:10 -0600

    initial tag parsing

---
+++ Parse.hs
@@ -0,0 +1,27 @@
+module Parse (imageLinks) where
+
+import Data.List (lookup)
+import Data.Maybe (mapMaybe)
+import Text.HTML.TagSoup
+
+tagNamed :: String -> Tag String -> Maybe (Tag String)
+tagNamed name t@(TagOpen tagName _) = if name == tagName 
+    then Just t 
+    else Nothing
+tagNamed name _ = Nothing
+
+tagAttributes :: Tag String -> Maybe [Attribute String]
+tagAttributes (TagOpen _ attributes) = Just attributes
+tagAttributes _                      = Nothing
+
+imageSource :: Tag String -> Maybe String
+imageSource tag = do
+  imageTag <- tagNamed "img" tag
+  attrs <- tagAttributes imageTag
+  lookup "src" attrs
+
+imageLinks :: String -> [String]
+imageLinks = process . parseTags
+  where
+    process :: [Tag String] -> [String]
+    process = mapMaybe imageSource
